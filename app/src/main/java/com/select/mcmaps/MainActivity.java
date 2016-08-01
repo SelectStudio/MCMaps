ff1b5ee0c92923c7514f94f6bcc7f8b68829cda5
megatronus <megatronus@sina.cn>
2016-08-01 11:51:36 +0800

    test

--- app/src/main/java/com/select/mcmaps/MainActivity.java
+++ app/src/main/java/com/select/mcmaps/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "2016/8/1 test", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
